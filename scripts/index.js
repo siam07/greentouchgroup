30456e6c40e3ecb2bafec9a65560dde7e6dfe9b0
siam <54509932+siam07@users.noreply.github.com>
2020-02-10 15:43:47 +0600

    update

--- scripts/index.js
+++ scripts/index.js
@@ -20,20 +20,32 @@ $(function(){
         $('body').bind("mousewheel", function() {
             return true;
         });
-        document.querySelector('.section1').style.display = 'block';
-        document.querySelector('.itm1').style.transform = 'translateY(0)';
-        document.querySelector('.itm2').style.transform = 'translateY(0)';
-        document.querySelector('.itm3').style.transform = 'translateY(0)';
-        document.querySelector('.itm4').style.transform = 'translateY(0)';
-        document.querySelector('.itm5').style.transform = 'translateY(0)';
-        document.querySelector('.itm1').style.opacity = '1';
-        document.querySelector('.itm2').style.opacity = '1';
-        document.querySelector('.itm3').style.opacity = '1';
-        document.querySelector('.itm4').style.opacity = '1';
-        document.querySelector('.itm5').style.opacity = '1';
-        document.querySelector('.logo').style.transform = 'translateY(0)';
-        document.querySelector('.logo').style.opacity = '1';
-    },4300);		
+        document.querySelector('.heading1').style.transform = 'translateY(0)';
+        document.querySelector('.paragraph1').style.transform = 'translateY(0)';
+        document.querySelector('.heading1').style.opacity = '1';
+        document.querySelector('.paragraph1').style.opacity = '1';
+        document.querySelector('.scrolltostart').style.display = 'block';
+        setTimeout(function(){
+        document.querySelector('.scrolltostart').style.transform = 'translateY(0)';
+        },500);
+        setTimeout(function(){
+            document.querySelector('.itm1').style.transform = 'translateY(0)';
+            document.querySelector('.itm2').style.transform = 'translateY(0)';
+            document.querySelector('.itm3').style.transform = 'translateY(0)';
+            document.querySelector('.itm4').style.transform = 'translateY(0)';
+            document.querySelector('.itm5').style.transform = 'translateY(0)';
+            document.querySelector('.itm1').style.opacity = '1';
+            document.querySelector('.itm2').style.opacity = '1';
+            document.querySelector('.itm3').style.opacity = '1';
+            document.querySelector('.itm4').style.opacity = '1';
+            document.querySelector('.itm5').style.opacity = '1';
+            document.querySelector('.logo').style.transform = 'translateY(0)';
+            document.querySelector('.logo').style.opacity = '1';
+            setTimeout(function(){
+                document.querySelector('.section1').style.display = 'block';
+            },2000);
+        },1000);
+    },4100);		
 });
 
 
